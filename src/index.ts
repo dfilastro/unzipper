b99f87ed0e12274b018435998e0a7bd51558c999
Diego Filastro <d.filastro@hotmail.com>
2023-01-25 00:05:56 -0300

    fix: fix problem with headers

--- src/index.ts
+++ src/index.ts
@@ -1,219 +1,175 @@
 import fs from 'fs';
-import { Readable } from 'stream';
-import readline from 'readline';
 import jszip from 'jszip';
 import xlsx from 'xlsx';
 
-import assert from 'assert';
-import { parse } from 'csv-parse';
-
 import RowToColumn from '../utils/rowToColumn';
 import ReadFile from '../utils/readFile';
+import getEqualIndex from '../utils/getEqualIndex';
+import getIncludedIndex from '../utils/getIncludedIndex';
+import lowerToHigher from '../utils/compareNumber';
+import columnToRow from '../utils/columnToRow';
 
 const fct = async () => {
-  let files = [] as string[];
-  try {
-    fs.readdirSync('./').forEach((file) => {
-      if (file.includes('.zip')) {
-        files.push(file);
+  let dataFileArray = '' as any;
+  const files = [] as string[];
+  const csvFiles = [] as string[];
+  let dataFile = [] as any;
+
+  fs.readdirSync('./').forEach((file) => {
+    if (file.includes('.zip')) {
+      files.push(file);
+    }
+  });
+
+  const filesCreation = files.map(async (i, index) => {
+    const fileContent = ReadFile(`${i}`) as Buffer;
+    const jszipInstance = new jszip();
+    const result = await jszipInstance.loadAsync(fileContent);
+    const keys = Object.keys(result.files);
+
+    for (let key of keys) {
+      const item = result.files[key];
+
+      if (key.toLocaleLowerCase().includes('result_lifetime') && !key.includes('DataFiles')) {
+        fs.writeFileSync(`${index + 1}-${key}`, Buffer.from(await item.async('arraybuffer')));
+      }
+    }
+  });
+
+  await Promise.all(filesCreation);
+
+  fs.readdirSync('./').forEach((file) => {
+    if (file.includes('.csv')) {
+      csvFiles.push(file);
+    }
+  });
+
+  csvFiles.map(async (i) => {
+    if (dataFile.length < csvFiles.length) {
+      return await dataFile.push(ReadFile(`${i}`)?.toString() as string);
+    }
+  });
+
+  // Create an array by splitting the CSV by newline
+  dataFile = await dataFile.toString()?.trim().split(/\r?\n/);
+
+  const outputs = [
+    '+++++++++ 1.1.1.2 Project summary table',
+    '+++++++++ 1.7.1 Project financing summary',
+    '+++++++++ 1.6.1 Investment Timeline',
+    '+++++++++ 2.2 Investment Timeline - Capacity',
+    '+++++++++ 1.6.5 Microgrid costs',
+    '+++++++++ 1.2 LifeTime Cost and Emissions',
+  ];
+
+  const newOutputObject = {} as any;
+  const blankIndex = getEqualIndex(dataFile, '');
+  outputs.map((i: any, _index) => {
+    newOutputObject[i] = getIncludedIndex(dataFile, i);
+  });
+
+  const desiredOutputs = [] as any;
+
+  for (let key in newOutputObject) {
+    newOutputObject[key].map((i: any) => {
+      newOutputObject[key].push(blankIndex[blankIndex.findIndex((el) => el > i)]);
+    });
+
+    newOutputObject[key].sort(lowerToHigher);
+
+    newOutputObject[key].map((i: any, index: any) => {
+      if (index % 2 === 0) {
+        newOutputObject[key][index] = dataFile?.slice(i, newOutputObject[key][index + 1]);
       }
     });
 
-    files.map(async (i, _index) => {
-      const fileContent = ReadFile(`${i}`) as any;
-      const jszipInstance = new jszip();
-      const result = await jszipInstance.loadAsync(fileContent);
-      const keys = Object.keys(result.files);
+    newOutputObject[key] = newOutputObject[key].filter((_i: any, index: any) => index % 2 === 0);
 
-      for (let key of keys) {
-        const item = result.files[key];
-
-        if (
-          item.name.toLocaleLowerCase().includes('result_lifetime') &&
-          !item.name.includes('DataFiles')
-        ) {
-          fs.writeFileSync(item.name, Buffer.from(await item.async('arraybuffer')));
-        }
-      }
-
-      const csvFilePath = 'Result_Lifetime002.csv';
-
-      //   let dataFile = ReadFile(`${csvFilePath}`)?.toString() as any;
-      let dataFile = ReadFile(`${csvFilePath}`) as any;
-      //   dataFile = dataFile.trim().split(/\r?\n/);
-
-      const outputs = [
-        '+++++++++ 1.1.1.2 Project summary table',
-        '+++++++++ 1.7.1 Project financing summary',
-        '+++++++++ 1.6.1 Investment Timeline',
-        '+++++++++ 2.2 Investment Timeline - Capacity',
-        '+++++++++ 1.2 LifeTime Cost and Emissions',
-        '+++++++++ 1.6.5 Microgrid costs',
-      ];
-
-      ////////////////////////////////////////////////////////////
-      var results = [] as any;
-
-      fs.createReadStream('Result_Lifetime002.csv')
-        .pipe(
-          parse({
-            relax_column_count: true,
-            delimiter: ['<KEYVALUE>', '<TABLE>', ',', '<HEADER>', '/\r?\n/'],
-          })
-        )
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-          console.log(results);
-        });
-
-      console.log(results);
-      //////////////////////////////////////////////////////////////////////////
-      // const readableFile = new Readable();
-      // readableFile.push(dataFile);
-      // readableFile.push(null);
-
-      // const fileLine = readline.createInterface({
-      //   input: readableFile,
-      // }) as any;
-
-      // const results = [] as any;
-      // let countLine = 0;
-
-      // // const desiredOutputs = outputs.map((i) => {
-      // //   const inicialIndex = dataFile.findIndex((el: string) => el.includes(i));
-      // //   const slicedArray = dataFile.slice(inicialIndex);
-      // //   const finalIndex = slicedArray.indexOf('');
-
-      // //   return slicedArray.slice(0, finalIndex);
-      // // });
-
-      // let indexes = [] as number[];
-
-      // for await (const line of fileLine) {
-      //   countLine++;
-      //   const fileLineSplit = line.trim().replace(/"/g, '').split(',');
-
-      //   // console.log(line);
-
-      //   if (line.includes(outputs[0])) {
-      //     indexes.push(countLine);
-      //   }
-
-      //   if (countLine > 30 && countLine < 34) {
-      //     // console.log(`${countLine} - ${line}`);
-
-      //     // console.log(`Passou aqui ${countLine} - ${line}`);
-      //     results.push(fileLineSplit);
-      //   }
-
-      //   if (line.includes(outputs[0])) {
-      //     // console.log(fileLineSplit.indexOf(line));
-      //   }
-
-      //   // console.log(fileLineSplit);
-      // }
-      // console.log(results);
-
-      //////////////////////////////////////////////////////////////////////////
-
-      //   // Create an array by splitting the CSV by newline
-      //   dataFile = dataFile.trim().split(/\r?\n/);
-
-      //   const outputs = [
-      //     '+++++++++ 1.1.1.2 Project summary table',
-      //     '+++++++++ 1.7.1 Project financing summary',
-      //     '+++++++++ 1.6.1 Investment Timeline',
-      //     '+++++++++ 2.2 Investment Timeline - Capacity',
-      //     '+++++++++ 1.2 LifeTime Cost and Emissions',
-      //     '+++++++++ 1.6.5 Microgrid costs',
-      //   ];
-
-      //   const desiredOutputs = outputs.map((i) => {
-      //     const inicialIndex = dataFile.findIndex((el: string) => el.includes(i));
-      //     const slicedArray = dataFile.slice(inicialIndex);
-      //     const finalIndex = slicedArray.indexOf('');
-
-      //     return slicedArray.slice(0, finalIndex);
-      //   });
-
-      //   RowToColumn(desiredOutputs, '+++++++++ 1.6.1 Investment Timeline');
-      //   RowToColumn(desiredOutputs, '+++++++++ 2.2 Investment Timeline - Capacity');
-
-      //   const results = [] as string[];
-
-      //   desiredOutputs.map((item) => {
-      //     const title = item[0].split(',')[0];
-      //     const innerKeys = item[1]?.trim().split(',');
-      //     innerKeys?.pop();
-      //     const isSummary = title.includes('summary');
-
-      //     for (let i = isSummary ? 1 : 2; i < item.length; i++) {
-      //       let tempRow = item[i].trim();
-
-      //       if (tempRow) {
-      //         tempRow = tempRow.split(',');
-      //         const tempTitle = tempRow[0]?.replace(/"/g, '');
-      //         const innerJSON = {} as any;
-      //         innerJSON[title?.replace(/"/g, '')] = tempTitle;
-
-      //         for (let j = 1; j < tempRow.length; j++) {
-      //           if (isSummary) {
-      //             innerJSON[`Scenario ${j}`] = Number(tempRow[j]);
-      //           } else {
-      //             innerJSON[innerKeys[j]?.replace(/"/g, '')] = !Number(tempRow[j])
-      //               ? 0
-      //               : Number(tempRow[j]);
-      //           }
-      //         }
-
-      //         results.push(innerJSON);
-      //       }
-      //     }
-      //   });
-
-      //   let workbook = xlsx.utils.book_new();
-      //   let worksheet = xlsx.utils.aoa_to_sheet([]);
-      //   // let worksheet = xlsx.utils.json_to_sheet([]);
-
-      //   xlsx.utils.book_append_sheet(workbook, worksheet);
-
-      //   const headers = [] as string[][];
-      //   let line = 1;
-
-      //   results.map((i, _index) => {
-      //     if (!headers[headers.length - 1]) headers.push(Object.keys(i));
-
-      //     if (Object.keys(i)[0] !== headers[headers.length - 1][0]) {
-      //       headers.push(Object.keys(i));
-      //     }
-      //   });
-
-      //   headers.map((item, _ind) => {
-      //     xlsx.utils.sheet_add_aoa(worksheet, [item], { origin: `A${line}` });
-      //     line++;
-
-      //     results.map((i, _index) => {
-      //       if (Object.keys(i)[0] === item[0]) {
-      //         xlsx.utils.sheet_add_aoa(worksheet, [Object.values(i)], { origin: `A${line}` });
-      //         line++;
-      //       }
-      //     });
-      //     results.shift();
-      //   });
-
-      //   // xlsx.writeFile(workbook, `${i.slice(0, -4)}_Output.xlsx`);
-
-      //   fs.unlink(csvFilePath, (err) => {
-      //     if (err) {
-      //       throw err;
-      //     }
-      //   });
-      //   xlsx.writeFile(workbook, `File_Output.xlsx`);
-    });
-  } catch (error) {
-    // console.log('Something went wrong, check out the messages above');
-    console.log(error);
+    desiredOutputs.push(...newOutputObject[key]);
   }
+
+  RowToColumn(desiredOutputs, '+++++++++ 2.2 Investment Timeline - Capacity');
+  RowToColumn(desiredOutputs, '+++++++++ 1.6.1 Investment Timeline');
+
+  const results = [] as string[];
+
+  // CREATE A FUNCTION ///////////////////////////////////////////////////////////
+  // PASS JUST ITEMS 1.6.5 AND 1.2 ///////////////////////////////////////////////
+  let counterScenario = 1;
+
+  desiredOutputs.map((i: any, _index: number) => {
+    if (counterScenario > desiredOutputs.length / outputs.length) counterScenario = 1;
+
+    i.splice(2, 0, `"Scenario ${counterScenario}"`);
+    counterScenario++;
+  });
+
+  columnToRow(desiredOutputs, '+++++++++ 2.2 Investment Timeline - Capacity');
+  columnToRow(desiredOutputs, '+++++++++ 1.1.1.2 Project summary table');
+  columnToRow(desiredOutputs, '+++++++++ 1.7.1 Project financing summary');
+  columnToRow(desiredOutputs, '+++++++++ 1.6.1 Investment Timeline');
+
+  // IT ONLY WORKS WITH 2 OR MORE FILES /////////////////////
+  desiredOutputs.map((item: any) => {
+    const title = item[0].split(',')[0];
+    const innerKeys = item[1]?.trim().split(',');
+    innerKeys?.pop();
+
+    for (let i = 2; i < item.length; i++) {
+      let tempRow = item[i]?.trim();
+
+      if (tempRow) {
+        tempRow = tempRow.split(',');
+        const tempTitle = tempRow[0]?.replace(/"/g, '');
+        const innerJSON = {} as any;
+        innerJSON[title?.replace(/"/g, '')] = tempTitle;
+
+        for (let j = 1; j < tempRow.length; j++) {
+          innerJSON[innerKeys[j]?.replace(/"/g, '')] = !Number(tempRow[j]) ? 0 : Number(tempRow[j]);
+          // }
+        }
+        results.push(innerJSON);
+      }
+    }
+  });
+
+  let workbook = xlsx.utils.book_new();
+  let worksheet = xlsx.utils.aoa_to_sheet([]);
+
+  xlsx.utils.book_append_sheet(workbook, worksheet);
+
+  const headers = [] as string[][];
+  let line = 1;
+
+  results.map((i, index) => {
+    if (!headers[headers.length - 1]) headers.push(Object.keys(i));
+    if (index > 0) {
+      if (Object.keys(i)[0] !== headers[headers.length - 1][0]) {
+        headers.push(Object.keys(i));
+      }
+    }
+  });
+
+  headers.map((item, _ind) => {
+    xlsx.utils.sheet_add_aoa(worksheet, [item], { origin: `A${line}` });
+    line++;
+
+    results.map((i, _index) => {
+      if (Object.keys(i)[0] === item[0]) {
+        xlsx.utils.sheet_add_aoa(worksheet, [Object.values(i)], { origin: `A${line}` });
+        line++;
+      }
+    });
+    results.shift();
+  });
+
+  // fs.unlink(csvFilePath, (err) => {
+  //   if (err) {
+  //     throw err;
+  //   }
+  // });
+
+  xlsx.writeFile(workbook, `Example_Output.xlsx`);
 };
 
 fct();
--- utils/columnToRow.ts
+++ utils/columnToRow.ts
@@ -1,5 +1,6 @@
 export default function RowToColumn(outputs: string[][], item: string) {
   let counter = 0;
+  let keyss = [] as any;
 
   outputs.map((i: string[], index: number) => {
     if (i[0].includes(item)) {
@@ -7,8 +8,6 @@ export default function RowToColumn(outputs: string[][], item: string) {
       counter++;
 
       i.splice(2, 1);
-
-      console.log(i);
 
       // HEADER
       finalArray.push(i[0]);
@@ -21,14 +20,35 @@ export default function RowToColumn(outputs: string[][], item: string) {
       keys.shift();
       keys.unshift('"Source"');
 
+      //  JOIN ALL THE KEYS AND REMOVE DUPLICATES
+      // new Set([...arr1. ...arr2])
+
+      // https://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript-and-de-duplicate-items
+
+      if (keyss.length === 0) {
+        keyss = [...keys];
+      }
+
+      if (keyss.length < keys.length) {
+        keyss = [...keys];
+      }
+
       // SUBHEADER
-      finalArray.push(keys.toString());
+      finalArray.push(keyss.toString());
 
       // BODY
       const values = i.map((item, _index) => {
         return item.split(',')[1];
       });
 
+      keys.map((i: any, index: number) => {
+        if (i !== keyss[index] && i !== '<HEADER>' && i !== '<KEYVALUE>') {
+          return values.splice(values.length - 1, 0, '0');
+        }
+      });
+
+      console.log(keys);
+      console.log(keyss);
       values.splice(0, 1, `"Scenario ${counter}"`);
       const stringValues = values.toString();
 
